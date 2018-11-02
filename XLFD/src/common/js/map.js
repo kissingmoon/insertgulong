c5db5a340233ae57a6cefdfe0088d9c0f42a43c7
masteronelight <masteronelight@gmail.com>
2018-11-02 19:09:08 +0800

    1

--- XLFD/src/common/js/map.js
+++ XLFD/src/common/js/map.js
@@ -92,12 +92,12 @@ export const httpUrl = {
         sendCode:'/user/sendCode',
         getRecomemendCpType:'/v5/gc/getRecommendCpType',
         getJumpConfig:'/v5/jumpConfig/getJumpConfig',
-        webSocket:'ws://192.168.9.42:8082/websocket'  ,
+        // webSocket:'ws://192.168.9.42:8082/websocket'  ,
         // webSocket:'ws://test.weinisi01.com:8090/jeeplus/websocket'  ,
         // webSocket:'ws://weinisi01.com/api/websocket',
-        // webSocket:'wss://www.xlfdapi.com/websocket',
-        // baseUrl:'/api',
-        baseUrl:'/test'
+        webSocket:'wss://www.xlfdapi.com/websocket',
+        baseUrl:'/api',
+        // baseUrl:'/test'
     },
     pay:{
         chargeUrl:'/v2/app-charge-url/get-charge-url',
